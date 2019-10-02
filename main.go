4a799039b56b2abf8e717ef692a00b30eae56460
Jorge Inostroza <jeinostroza@hotmail.com>
2019-10-02 11:46:39 -0500

    command line

--- main.go
+++ main.go
@@ -5,26 +5,30 @@ import (
 	"os"
 )
 
-func main() {
-
-	var locale string
+func translate(imput string) string {
 	var translations = make(map[string]string)
 	translations["en"] = "Hello"
 	translations["es"] = "Hola"
 	translations["fr"] = "Bonjour"
+	return translations[imput]
+}
 
+func argsswitch(input []string) string {
+	var locale string
 	if len(os.Args) == 1 {
 		fmt.Printf("Please, select a language(en,es,de)\n")
 		fmt.Scanln(&locale)
 	} else {
 		locale = os.Args[1]
 	}
+	return locale
+}
 
-	output := translations[locale]
+func main() {
+	locale := argsswitch(os.Args)
+	output := translate(locale)
 	if output == "" {
 		output = "Yo"
 	}
-
 	fmt.Printf(output + " Go\n")
-
 }
